f9e45f4b46d35b77906b2646d3f9dcef3c3188e1
M L <max@perfekd.de>
2023-12-28 01:55:19 +0100

    <leader>tg&th

--- lua/mxluk/remap.lua
+++ lua/mxluk/remap.lua
@@ -22,3 +22,35 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
 end)
+vim.keymap.set('n', '<leader>tg', function()
+    local word = vim.fn.expand('<cword>')
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local line_num = cursor_pos[1]
+
+    -- Replace the word with HTML tags
+    vim.api.nvim_command('normal ciw<' .. word .. '></' .. word .. '>')
+
+    -- Calculate the new cursor position, right after the opening tag
+    local new_col_pos = cursor_pos[2] - #word + #('<' .. word .. '>') + 1
+
+    -- Set the cursor position and enter insert mode
+    vim.api.nvim_win_set_cursor(0, {line_num, new_col_pos})
+    vim.api.nvim_command('startinsert')
+end)
+vim.keymap.set('n', '<leader>th', function()
+    local word = vim.fn.expand('<cword>')
+    local cursor_pos = vim.api.nvim_win_get_cursor(0)
+    local line_num = cursor_pos[1]
+    local indent = string.rep(" ", vim.fn.indent(line_num))
+
+    -- Replace the word with HTML tags and two line breaks
+    vim.api.nvim_command('normal ciw<' .. word .. '>\n' .. indent .. '    \n' .. indent .. '</' .. word .. '>')
+
+    -- Calculate the new cursor position at the indented empty line
+    local new_line_num = line_num + 1
+    local new_col_pos = #indent + 4 -- 4 spaces for the additional indentation
+
+    -- Set the cursor position and enter insert mode
+    vim.api.nvim_win_set_cursor(0, {new_line_num, new_col_pos})
+    vim.api.nvim_command('startinsert')
+end)
