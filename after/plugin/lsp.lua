221914375beba7e4f40599b3b7d41c7eaa6c7df9
Max Lukonin <max@perfekd.de>
2024-12-11 19:24:44 +0000

    fix things

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,14 +17,14 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-  }
+    ensure_installed = {'ts_ls', 'rust_analyzer'},
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end,
+    }
 })
 
 local cmp = require('cmp')
